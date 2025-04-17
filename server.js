c0f3890515cf201b5aab636042777119caa4ff79
Brian Acevedo <acevedobrian499@gmail.com>
2025-04-17 11:23:27 -0500

    Habilitar CORS

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Habilitar CORS para todas las rutas y orígenes
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('¡Hola desde el backend en Render!');
+  res.send('¡Hola desde el backend en Render con CORS habilitado!');
 });
 
 app.listen(port, () => {
